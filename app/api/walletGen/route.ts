ff1d8d0dbd443f1a96a44c25f7cbf13a5461cdb2
engageintellect <jc9361@gmail.com>
2024-03-26 23:08:24 -0700

    init

--- app/api/walletGen/route.ts
+++ app/api/walletGen/route.ts
@@ -3,7 +3,8 @@ export const dynamic = "force-dynamic";
 
 export async function GET() {
   const uniqueParam = new Date().getTime(); // Current timestamp as a cache buster
-  const res = await fetch(`http://engage-dev.com:4320/api/walletGen?_=${uniqueParam}`);
+  const res = await fetch(`https://engage-dev.com:4320/api/walletGen?_=${uniqueParam}`);
+  // const res = await fetch(`http://localhost:8000/api/walletGen?_=${uniqueParam}`);
   const wallet = await res.json();
 
   return new Response(JSON.stringify(wallet), {
